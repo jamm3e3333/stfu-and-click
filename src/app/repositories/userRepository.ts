61100b7281fda1bf822ce3de7ce60341cbcaeef4
Jakub Vala <jakub.vala@plu4u.net>
2022-12-11 18:36:16 +0100

    🐛 Fix getting user for id

--- src/app/repositories/userRepository.ts
+++ src/app/repositories/userRepository.ts
@@ -1,14 +1,14 @@
-import baseLogger from '../../logger'
 import firestore, { getDataFromQuerySnapshots } from '../database/firestore'
+import { FieldPath } from 'firebase-admin/firestore'
 import { E_CODES } from '../errors'
-import { ValidationError } from '../errors/classes'
+import { NotAuthorized, ValidationError } from '../errors/classes'
 
 const userRepository = firestore.collection('users')
 
-interface User {
+export interface User {
   id: string
   email: string
-  clickCount: number
+  clickCount?: number
   teamId: string
 }
 
@@ -27,10 +27,30 @@ export const createUser = async (user: Pick<User, 'email' | 'teamId'>) => {
 
 export const getUserForUserId = async (user: Pick<User, 'id'>) => {
   const [userData] = await getDataFromQuerySnapshots<User>(
-    userRepository.where('id', '==', user.id).limit(1).get()
+    userRepository.where(FieldPath.documentId(), '==', user.id).limit(1).get()
   )
   return userData
 }
 
-export const getUsersByTeamId = async (user: Pick<User, 'teamId'>) =>
-  userRepository.where('teamId', '==', user.teamId).get()
+export const getUsersForTeamIds = async (teamIds: string[]) =>
+  getDataFromQuerySnapshots<User>(
+    userRepository.where('teamId', 'in', teamIds).get()
+  )
+
+export const updateUserClickForTeam = async (
+  userId: string,
+  teamId: string
+) => {
+  const user = await userRepository
+    .where(FieldPath.documentId(), '==', userId)
+    .where('teamId', '==', teamId)
+    .get()
+  if (!user) {
+    throw new NotAuthorized()
+  }
+  return user.forEach(async u => {
+    await u.ref.update({
+      clickCount: Number(u.data()?.clickCount) + 1 ?? 1,
+    })
+  })
+}
