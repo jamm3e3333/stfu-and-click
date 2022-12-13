5073ea900f2e9ceb00442ab13a336d46d381674a
Jakub Vala <jakub.vala@plu4u.net>
2022-12-13 21:31:33 +0100

    ✨ Modify firestore for testing

--- src/app/database/firestore.ts
+++ src/app/database/firestore.ts
@@ -1,11 +1,26 @@
 import * as firebase from 'firebase-admin'
 import config from '../../config'
+import { Firestore } from '@google-cloud/firestore'
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(config.firebase.serviceAccount),
-})
+const getFirestoreForEmulator = () => {
+  const firestore = new Firestore()
+  firestore.settings({
+    projectId: 'test',
+    credentials: {},
+  })
+  return firestore
+}
 
-export default firebase.firestore()
+// For tests use Firestore emulator without specifying SA
+const firestore = config.enableTests
+  ? getFirestoreForEmulator()
+  : firebase
+      .initializeApp({
+        credential: firebase.credential.cert(config.firebase.serviceAccount),
+      })
+      .firestore()
+
+export default firestore
 
 export const getDataFromQuerySnapshots = async <T>(
   snapshots: Promise<FirebaseFirestore.QuerySnapshot>
