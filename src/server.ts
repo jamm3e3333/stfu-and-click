2f01edbbf4c0c54732238cec00833f39f285320a
Jakub Vala <jakub.vala@plu4u.net>
2022-12-13 21:29:51 +0100

    🚚 Move logger to app folder

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import * as ctrl from './app/controllers'
 import * as userService from './app/services/userService'
 import * as teamService from './app/services/teamService'
 import * as OpenApiValidator from 'express-openapi-validator'
-import logger from './logger'
+import logger from './app/logger'
 import httpErrorResponder from './app/controllers/httpErrorResponder'
 
 const validator = OpenApiValidator.middleware({
   apiSpec: './docs/api/openapi.yaml',
+  ignorePaths: /\/{1}$/,
 })
 
 const server = express()
@@ -16,13 +17,14 @@ server.use(ctrl.cors)
 server.use(logger.express)
 server.use(validator)
 
+server.all('/', ctrl.httpRootHandler)
 server.post('/api/v1/users', ctrl.service(userService.handlePostUser))
 server.post('/api/v1/sessions', ctrl.service(userService.handlePostSession))
 
 server.get('/api/v1/teams', ctrl.service(teamService.handleGetTeams))
 server.get('/api/v1/teams/:teamId', ctrl.service(teamService.handleDetailTeam))
 server.post(
-  '/api/v1/addTeamClick/:teamId',
+  '/api/v1/teamClick/:teamId',
   ctrl.service(teamService.handlePostClickForTeam)
 )
 
